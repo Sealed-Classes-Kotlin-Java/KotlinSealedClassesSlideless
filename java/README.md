afd510be6ad82bcbb35079f62b40a9b0c4268e55
Lothar Schulz <lothar.sch@gmx.de>
2022-03-10 22:41:27 +0100

    chore fatJar

--- java/README.md
+++ java/README.md
@@ -1,4 +1,7 @@
 
 ```sh
 ./gradlew clean build
+
+./gradlew clean build && java -jar app/build/dist/app.jar # --organization="Sealed-Classes-Kotlin-Java"
+./gradlew clean build && java -jar app/build/dist/app.jar # -o Sealed-Classes-Kotlin-Java
 ```
--- java/app/build.gradle.kts
+++ java/app/build.gradle.kts
@@ -10,14 +10,42 @@ dependencies {
     // java
     implementation("com.google.guava:guava:30.1.1-jre")
 
-    // test: TestNG framework, also requires calling test.useTestNG() below
+    // testNG
     testImplementation("org.testng:testng:7.4.0")
+
+    // github api
+    implementation("org.kohsuke:github-api:1.301")
+
+    // cli
+    implementation("com.github.ajalt.clikt:clikt:3.4.0")
 }
 
-application {
-    mainClass.set("info.lotharschulz.github.org.verifier.App")
+val classMain = "info.lotharschulz.github.org.verifier.App"
+
+val fatJar = task("customFatJar", type = Jar::class) {
+    duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+    manifest {
+        attributes["Implementation-Title"] = "GitHub repository verifier"
+        attributes["Implementation-Version"] = archiveVersion
+        attributes["Main-Class"] = classMain
+    }
+    archiveFileName.set("app.jar")
+    from(configurations.runtimeClasspath.get().map { if (it.isDirectory) it else zipTree(it) })
+    with(tasks.jar.get() as CopySpec)
+    destinationDirectory.set(layout.buildDirectory.dir("dist"))
+}
+
+tasks.getByName("build").dependsOn("customFatJar")
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
 }
 
 tasks.named<Test>("test") {
     useTestNG()
+}
+
+application {
+    mainClass.set(classMain)
 }
