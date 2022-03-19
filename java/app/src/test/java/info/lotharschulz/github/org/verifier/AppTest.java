33aa40ff0bd62d62ad658a018195322d73e84cfa
Lothar Schulz <lothar.sch@gmx.de>
2022-03-19 21:14:28 +0100

    chore test changed

--- java/app/src/test/java/info/lotharschulz/github/org/verifier/AppTest.java
+++ java/app/src/test/java/info/lotharschulz/github/org/verifier/AppTest.java
@@ -1,11 +1,11 @@
 package info.lotharschulz.github.org.verifier;
 
+import info.lotharschulz.github.org.verifier.api.github.Utils;
 import org.testng.annotations.*;
 import static org.testng.Assert.*;
 
 public class AppTest {
-    @Test public void repoScannerHasCall() throws Exception {
-        RepositoryScanner classUnderTest = new RepositoryScanner();
-//        assertNotNull(classUnderTest.call(), "RepositoryScan should have a call method");
+    @Test public void utilsHasVerifyGitHubConnection() throws Exception {
+        assertNotNull(Utils.verifyGitHubConnection("empty"), "Utils should have a verifyGitHubConnection method");
     }
 }
