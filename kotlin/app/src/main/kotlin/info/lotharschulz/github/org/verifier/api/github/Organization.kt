c1596b054b95719d4eba54321e10826046da253a
Lothar Schulz <lothar@miro.com>
2022-05-04 16:56:19 +0200

    chore Organization is part of the journey and not part of the starting branch

--- kotlin/app/src/main/kotlin/info/lotharschulz/github/org/verifier/api/github/Organization.kt
+++
@@ -1,8 +0,0 @@
-package info.lotharschulz.github.org.verifier.api.github
-
-import org.kohsuke.github.GHOrganization
-
-sealed class Organization {
-    data class Success(val githubOrg: GHOrganization) : Organization()
-    data class Failure(val error: String) : Organization()
-}
